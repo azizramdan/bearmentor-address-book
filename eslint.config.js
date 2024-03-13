d7c8437e1eabcf51728c3fa8fb3dc7e4c862b2f6
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2024-03-13 17:04:08 +0700

    refactor: add curly rules eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,11 @@
 const antfu = require('@antfu/eslint-config').default
 
 module.exports = antfu({
+  rules: {
+    'curly': ['error', 'all'],
+    'no-alert': ['off'],
+    'style/brace-style': ['error', '1tbs'],
+  },
   formatters: {
     html: true,
     css: true,
