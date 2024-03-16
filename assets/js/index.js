a228530046881c955b49be03e9d2e93c80c8bc12
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2024-03-16 17:23:39 +0700

    refactor: rewrite parse html with array map

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,20 @@
 (() => {
+  $eventBus.on(EVENT_LABELS_UPDATED, refresh)
+  $eventBus.on(EVENT_CONTACTS_UPDATED, refresh)
+
   const contactModel = new Contact()
   let contacts = []
   let favorites = []
 
   refresh()
   initEvents()
-  $eventBus.on(EVENT_LABELS_UPDATED, refresh)
-  $eventBus.on(EVENT_CONTACTS_UPDATED, refresh)
 
   function refresh() {
     contacts = contactModel.index()
     favorites = contacts.filter(contact => contact.isFavorite)
-    renderCounter()
+    renderContactsCounter()
     renderFavoritesSection()
-    renderContacts()
+    renderContactsSection()
   }
 
   function initEvents() {
@@ -34,7 +35,7 @@
     })
   }
 
-  function renderCounter() {
+  function renderContactsCounter() {
     document.getElementById('contacts-count').textContent = contacts.length
   }
 
@@ -44,175 +45,144 @@
     if (favorites.length) {
       favoritesSection.classList.remove('hidden')
 
-      renderFavoritesCount()
-      renderFavorites()
+      document.getElementById('favorites-count').textContent = favorites.length
+      document.getElementById('favorites-container').innerHTML = favorites.map(favorite => parseContactTemplate(favorite)).join('')
     } else {
       favoritesSection.classList.add('hidden')
     }
   }
 
-  function renderFavoritesCount() {
-    document.getElementById('favorites-count').textContent = favorites.length
+  function renderContactsSection() {
+    document.getElementById('contacts-container').innerHTML = contacts.map(contact => parseContactTemplate(contact)).join('')
   }
 
-  function renderFavorites() {
-    let html = ''
-
-    favorites.forEach((favorite) => {
-      html += parseTemplate(favorite)
-    })
-
-    document.getElementById('favorites-container').innerHTML = html
-  }
-
-  function renderContacts() {
-    let html = ''
-
-    contacts.forEach((favorite) => {
-      html += parseTemplate(favorite)
-    })
-
-    document.getElementById('contacts-container').innerHTML = html
-  }
-
-  function parseTemplate(contact) {
-    const favoriteButton = parseFavoriteButton(contact)
-    const labelsButton = parseLabelsButton(contact.labels)
-
+  function parseContactTemplate(contact) {
     return /* html */ `
-    <div class="group flex justify-between hover:cursor-pointer">
-      <div
-        class="grow pl-3 grid items-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 hover:bg-sky-50 peer"
-      >
-        <div>${contact.firstName} ${contact.middleName} ${contact.lastName}</div>
-        <a
-          href="mailto:${contact.emails[0]?.mail}"
-          class="hidden sm:block w-fit hover:text-blue-600"
+      <div class="group flex justify-between hover:cursor-pointer">
+        <div
+          class="grow pl-3 grid items-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 hover:bg-sky-50 peer"
         >
-          ${contact.emails[0]?.mail}
-        </a>
-        <div class="hidden md:block">${contact.phoneNumbers[0]?.number}</div>
-        <div class="hidden lg:block">${contact.jobTitle}, ${contact.company}</div>
-        <div class="hidden xl:flex gap-1">${labelsButton}</div>
+          <div>${contact.firstName} ${contact.middleName} ${contact.lastName}</div>
+          <a
+            href="mailto:${contact.emails[0]?.mail}"
+            class="hidden sm:block w-fit hover:text-blue-600"
+          >
+            ${contact.emails[0]?.mail}
+          </a>
+          <div class="hidden md:block">${contact.phoneNumbers[0]?.number}</div>
+          <div class="hidden lg:block">${contact.jobTitle}, ${contact.company}</div>
+          <div class="hidden xl:flex gap-1">
+            ${
+              contact.labels.map(label => /* html */`
+                <a
+                  href="#"
+                  class="rounded-lg border border-slate-300 px-3 py-2 hover:bg-slate-200"
+                >
+                  ${label.name}
+                </a>
+              `).join('')
+            }
+          </div>
+        </div>
+        <div
+          class="invisible group-hover:visible flex justify-end peer-hover:bg-sky-50 w-32"
+        >
+          ${
+            contact.isFavorite
+              ? /* html */`
+                <button
+                  class="remove-from-favorites rounded-full hover:bg-gray-200 p-3"
+                  title="Remove from favorites"
+                  data-id="${contact.id}"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    class="icon icon-tabler icons-tabler-filled icon-tabler-star text-blue-700"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path
+                      d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z"
+                    />
+                  </svg>
+                </button>
+              `
+              : /* html */`
+                <button
+                  class="add-to-favorites rounded-full hover:bg-gray-200 p-3"
+                  title="Add to favorites"
+                  data-id="${contact.id}"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="icon icon-tabler icons-tabler-outline icon-tabler-star"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path
+                      d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"
+                    />
+                  </svg>
+                </button>
+            `
+          }
+          <button
+            class="rounded-full hover:bg-gray-200 p-3"
+            title="Edit content"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="icon icon-tabler icons-tabler-outline icon-tabler-pencil"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path
+                d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4"
+              />
+              <path d="M13.5 6.5l4 4" />
+            </svg>
+          </button>
+          <button
+            class="rounded-full hover:bg-gray-200 p-3"
+            title="More actions"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="icon icon-tabler icons-tabler-outline icon-tabler-dots-vertical"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+              <path d="M12 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+              <path d="M12 5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+            </svg>
+          </button>
+        </div>
       </div>
-      <div
-        class="invisible group-hover:visible flex justify-end peer-hover:bg-sky-50 w-32"
-      >
-        ${favoriteButton}
-        <button
-          class="rounded-full hover:bg-gray-200 p-3"
-          title="Edit content"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="icon icon-tabler icons-tabler-outline icon-tabler-pencil"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path
-              d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4"
-            />
-            <path d="M13.5 6.5l4 4" />
-          </svg>
-        </button>
-        <button
-          class="rounded-full hover:bg-gray-200 p-3"
-          title="More actions"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="icon icon-tabler icons-tabler-outline icon-tabler-dots-vertical"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
-            <path d="M12 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
-            <path d="M12 5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
-          </svg>
-        </button>
-      </div>
-    </div>
-    `
-  }
-
-  function parseFavoriteButton(contact) {
-    return contact.isFavorite
-      ? /* html */`
-        <button
-          class="remove-from-favorites rounded-full hover:bg-gray-200 p-3"
-          title="Remove from favorites"
-          data-id="${contact.id}"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="icon icon-tabler icons-tabler-filled icon-tabler-star text-blue-700"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path
-              d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z"
-            />
-          </svg>
-        </button>
       `
-      : /* html */`
-        <button
-          class="add-to-favorites rounded-full hover:bg-gray-200 p-3"
-          title="Add to favorites"
-          data-id="${contact.id}"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="icon icon-tabler icons-tabler-outline icon-tabler-star"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path
-              d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"
-            />
-          </svg>
-        </button>
-      `
-  }
-
-  function parseLabelsButton(labels) {
-    let html = ''
-
-    labels.forEach((label) => {
-      html += /* html */`
-        <a
-          href="#"
-          class="rounded-lg border border-slate-300 px-3 py-2 hover:bg-slate-200"
-        >
-          ${label.name}
-        </a>
-      `
-    })
-
-    return html
   }
 })()
--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,4 +1,6 @@
 (() => {
+  $eventBus.on(EVENT_LABELS_UPDATED, renderLabels)
+
   const navToggleEl = document.getElementById('nav-toggle')
   const navEl = document.getElementById('nav')
 
@@ -7,12 +9,103 @@
   }
 
   navToggleEl.addEventListener('click', toggle)
-  renderContactsCount()
+  renderContactsCounter()
   renderLabels()
   initCreateLabelsModal()
   initRenameLabelsModal()
 
-  $eventBus.on(EVENT_LABELS_UPDATED, renderLabels)
+  function toggle() {
+    navEl.classList.toggle('nav-closed')
+    navEl.classList.toggle('nav-open')
+  }
+
+  function renderContactsCounter() {
+    document.getElementById('nav-contacts-count').textContent = (new Contact()).index().length
+  }
+
+  function renderLabels() {
+    document.getElementById('nav-labels').innerHTML = (new Label())
+      .indexWithTotalContacts()
+      .map(label => /* html */ `
+        <a
+          href="#"
+          class="flex items-center justify-between rounded-full hover:bg-slate-200 w-full py-1 pl-5 font-semibold group h-14"
+        >
+          <span class="flex items-center gap-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="icon icon-tabler icons-tabler-filled icon-tabler-bookmark rotate-90"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path
+                d="M14 2a5 5 0 0 1 5 5v14a1 1 0 0 1 -1.555 .832l-5.445 -3.63l-5.444 3.63a1 1 0 0 1 -1.55 -.72l-.006 -.112v-14a5 5 0 0 1 5 -5h4z"
+              />
+            </svg>
+            ${label.name}
+          </span>
+          <div>
+            <div class="hidden group-hover:flex">
+              <button
+                class="rename-label-button rounded-full hover:bg-gray-300 p-3"
+                title="Rename label"
+                data-id="${label.id}"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="icon icon-tabler icons-tabler-outline icon-tabler-pencil"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path
+                    d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4"
+                  />
+                  <path d="M13.5 6.5l4 4" />
+                </svg>
+              </button>
+              <button
+                class="rounded-full hover:bg-gray-300 p-3"
+                title="Delete label"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="icon icon-tabler icons-tabler-outline icon-tabler-trash"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M4 7l16 0" />
+                  <path d="M10 11l0 6" />
+                  <path d="M14 11l0 6" />
+                  <path
+                    d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"
+                  />
+                  <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                </svg>
+              </button>
+            </div>
+            <small class="group-hover:hidden mr-5">${label.totalContacts}</small>
+          </div>
+        </a>
+      `)
+      .join('')
+  }
 
   function initCreateLabelsModal() {
     const modal = document.getElementById('create-label-modal')
@@ -66,107 +159,5 @@
       event.target.reset()
       $eventBus.emit(EVENT_LABELS_UPDATED)
     })
-  }
-
-  function toggle() {
-    navEl.classList.toggle('nav-closed')
-    navEl.classList.toggle('nav-open')
-  }
-
-  function renderContactsCount() {
-    document.getElementById('nav-contacts-count').textContent = (new Contact()).index().length
-  }
-
-  function renderLabels() {
-    const labels = (new Label()).indexWithTotalContacts()
-
-    html = ''
-
-    labels.forEach((label) => {
-      html += parseLabelMenu(label)
-    })
-
-    document.getElementById('nav-labels').innerHTML = html
-  }
-
-  function parseLabelMenu(label) {
-    return /* html */ `
-      <a
-        href="#"
-        class="flex items-center justify-between rounded-full hover:bg-slate-200 w-full py-1 pl-5 font-semibold group h-14"
-      >
-        <span class="flex items-center gap-3">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="icon icon-tabler icons-tabler-filled icon-tabler-bookmark rotate-90"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path
-              d="M14 2a5 5 0 0 1 5 5v14a1 1 0 0 1 -1.555 .832l-5.445 -3.63l-5.444 3.63a1 1 0 0 1 -1.55 -.72l-.006 -.112v-14a5 5 0 0 1 5 -5h4z"
-            />
-          </svg>
-          ${label.name}
-        </span>
-        <div>
-          <div class="hidden group-hover:flex">
-            <button
-              class="rename-label-button rounded-full hover:bg-gray-300 p-3"
-              title="Rename label"
-              data-id="${label.id}"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="icon icon-tabler icons-tabler-outline icon-tabler-pencil"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path
-                  d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4"
-                />
-                <path d="M13.5 6.5l4 4" />
-              </svg>
-            </button>
-            <button
-              class="rounded-full hover:bg-gray-300 p-3"
-              title="Delete label"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="icon icon-tabler icons-tabler-outline icon-tabler-trash"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M4 7l16 0" />
-                <path d="M10 11l0 6" />
-                <path d="M14 11l0 6" />
-                <path
-                  d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"
-                />
-                <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
-              </svg>
-            </button>
-          </div>
-          <small class="group-hover:hidden mr-5">${label.totalContacts}</small>
-        </div>
-      </a>
-    `
   }
 })()
